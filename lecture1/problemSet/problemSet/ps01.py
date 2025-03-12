ba96f184743abfaae5d0a6321328ff944b5b8f18
mi5ha6in <mi5ha6in@gmail.com>
2025-03-12 04:33:51 +0300

    adds problem set 0, ver 2

---
+++ lecture1/problemSet/problemSet/ps01.py
@@ -0,0 +1,11 @@
+# Problem Set 0
+# Name: mi5ha6in
+# Time Spent: 0:30
+
+import numpy
+
+x = int(input("Enter number x: "))
+y = int(input("Enter number y: "))
+
+print("x**y =", x**y)
+print("log(x) =", numpy.log2(8))
